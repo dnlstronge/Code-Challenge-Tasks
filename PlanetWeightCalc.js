b46be934d21dbf82280aba5d338a477af935e92a
dnlstronge <114242578+dnlstronge@users.noreply.github.com>
2022-10-01 15:57:00 +0100

    Create PlanetWeightCalc.js
    Simple calculator which works out weight of a given value on a given planet

---
+++ PlanetWeightCalc.js
@@ -0,0 +1,19 @@
+// Calculates weight of user input on specified planet, returns error message if no /incorrect planet entered
+
+const calculateWeight = (earthWeight, planet) => {
+switch (planet) {
+      case 'Mercury':
+          return earthWeight * .378;
+      case 'Venus':
+          return earthWeight * .907;
+      case 'Mars':
+          return earthWeight * .377;
+      case 'Jupiter':
+          return earthWeight * 2.36;
+      case 'Saturn':
+          return earthWeight * .916;
+      default: 
+          return 'Invalid Planet Entry. Try: Mercury, Venus, Mars, Jupiter or Saturn.'
+          
+}
+};
