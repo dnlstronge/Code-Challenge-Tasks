80b45699e79d5b2b28057cd5aca34358799b8301
dnlstronge <114242578+dnlstronge@users.noreply.github.com>
2022-10-02 09:18:32 +0100

    Create Sentence.js

---
+++ Sentence.js
@@ -0,0 +1,13 @@
+//Write a function with 3 parameters which use user input words to complete the sentence
+
+const sillySentence = (word1, word2, word3) => {
+  return ('I am so ' + word1 + ' because I ' + word2 + ' coding! Time to write some more awesome ' + word3 + '!')
+};
+
+
+//test
+
+console.log(sillySentence('excited', 'love', 'functions')) 
+
+// returns I am so excited because I love coding! Time to write some more awesome functions!
+
