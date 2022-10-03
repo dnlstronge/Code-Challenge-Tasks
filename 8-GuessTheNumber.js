330ac0269c3008cb29bb27107b4e2d5b2f537761
dnlstronge <114242578+dnlstronge@users.noreply.github.com>
2022-10-03 20:32:43 +0100

    Update 8-GuessTheNumber.js
    corrected solution

--- 8-GuessTheNumber.js
+++ 8-GuessTheNumber.js
@@ -1,30 +1,24 @@
-
-
-const generateTarget = () => {
-  return Math.floor(Math.random()* 9 + 1)}
-
-const compareGuesses = (userGuess, computerGuess, generateTarget) => { 
-  if (Math.abs(generateTarget - userGuess) <= Math.abs(generateTarget - userGuess)) {
-    return true }
-    else {
-   return false 
-  }
-}
-updateScore = (winner) => {
-  if (winner = 'human') {
-    let humanScore = +1
-  } else {
-  let computerScore = +1
-  }
-}
-
 let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-const advanceRound = (currentRoundNumber) => {
-  if (currentRoundNumber > 1)
-  {let currrentRoundNumber = +1; }
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
 }
 
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+  
+  const humanDifference = Math.abs(targetGuess - humanGuess)
+  const computerDifference = Math.abs(targetGuess - computerGuess)
+  return humanDifference <= computerDifference;
+}
 
+const updateScore = winner => {
+  if (winner === 'human') {
+    humanScore++;
+  } else if (winner === 'computer') {
+    computerScore++;
+  }
+}
+
+const advanceRound = () => currentRoundNumber++;
