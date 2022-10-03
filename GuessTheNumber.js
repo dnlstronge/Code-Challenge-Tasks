4cbf3fb5f6e32288ee38b68d6c0a563c23d3f3b4
dnlstronge <114242578+dnlstronge@users.noreply.github.com>
2022-10-03 17:43:43 +0100

    Create GuessTheNumber.js
    started - number generation functional

---
+++ GuessTheNumber.js
@@ -0,0 +1,8 @@
+const generateTarget = () => {
+  return Math.floor(Math.random()* 9 + 1)} 
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// tested - functional
+console.log(generateTarget())
