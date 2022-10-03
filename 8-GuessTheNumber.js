5a8ebdeeb57bb347553c1deed9f88b002c6e111d
dnlstronge <114242578+dnlstronge@users.noreply.github.com>
2022-10-03 20:27:08 +0100

    Update 8-GuessTheNumber.js
    Not functioning - getting correct RNG but have fault in boolean logic

--- 8-GuessTheNumber.js
+++ 8-GuessTheNumber.js
@@ -1,8 +1,30 @@
+
+
 const generateTarget = () => {
-  return Math.floor(Math.random()* 9 + 1)} 
+  return Math.floor(Math.random()* 9 + 1)}
+
+const compareGuesses = (userGuess, computerGuess, generateTarget) => { 
+  if (Math.abs(generateTarget - userGuess) <= Math.abs(generateTarget - userGuess)) {
+    return true }
+    else {
+   return false 
+  }
+}
+updateScore = (winner) => {
+  if (winner = 'human') {
+    let humanScore = +1
+  } else {
+  let computerScore = +1
+  }
+}
+
 let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// tested - functional
-console.log(generateTarget())
+const advanceRound = (currentRoundNumber) => {
+  if (currentRoundNumber > 1)
+  {let currrentRoundNumber = +1; }
+}
+
+
