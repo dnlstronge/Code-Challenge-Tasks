3c5e4d39d1462b09587fed9babf66eb8b8ff1a15
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-01-11 09:34:49 +0000

    Update JS-spread.js

--- JS-spread.js
+++ JS-spread.js
@@ -1,2 +1,10 @@
 // create an array with elements of 1, 2, 3, 4. Use the spread operator to create a new array with the same numbers & 5 and 6.
 
+
+const numbers = [1, 2, 3, 4];
+const newNum = [...numbers, 5, 6];
+
+console.log(newNum)
+
+
+//outputs: [1, 2, 3, 4, 5, 6]
