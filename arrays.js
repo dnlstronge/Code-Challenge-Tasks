0fa162c8abf65ccabf7e0fabf7dc5947671fd5ed
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-01-11 10:48:03 +0000

    Update and rename arrays to arrays.js

---
+++ arrays.js
@@ -0,0 +1,9 @@
+// given an array of numbers 1 - 5, create a new array where each element is multiplied by two:
+
+const numbers = [1, 2, 3, 4, 5];
+
+const newNum = numbers.map( (e) => e*2 );
+
+console.log(newNum);
+
+//output: [2, 4, 6, 8, 10]
