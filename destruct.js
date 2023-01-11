d32e3674d19ed1e314c1d7045c021830302ebbcf
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-01-11 10:21:58 +0000

    Update destruct.js

--- destruct.js
+++ destruct.js
@@ -1,1 +1,9 @@
 // create an array and destructure the first two elements, use numbers for elements and call them num1, num2 respectively:
+
+const numbers = [1, 2, 3, 4, 5];
+
+[num1, num2] = numbers;
+
+console.log(num1, num2)
+
+//output '1 2'
