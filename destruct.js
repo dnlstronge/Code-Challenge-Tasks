83537725b83fae58a8d916e46cb790bc4352e806
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-01-11 10:25:43 +0000

    Update destruct.js

--- destruct.js
+++ destruct.js
@@ -4,6 +4,16 @@ const numbers = [1, 2, 3, 4, 5];
 
 [num1, num2] = numbers;
 
-console.log(num1, num2)
+console.log(num1, num2);
 
 //output '1 2'
+
+
+
+// now grab the first and 4th number:
+
+[num1, , , num4] = numbers;
+
+console.log(num1, num4);
+
+//output: '1 4' (be mindful of the semi-colon!)
